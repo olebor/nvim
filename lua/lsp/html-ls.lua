858f471631f806400bc0ff8a4edda701d0975f62
Ole Johnny Borgersen <ole@appfarm.io>
2021-03-27 23:10:36 +0000

    Update

--- lua/lsp/html-ls.lua
+++ lua/lsp/html-ls.lua
@@ -6,4 +6,4 @@ require'lspconfig'.html.setup {
     cmd = {"node", DATA_PATH .. "/lspinstall/html/vscode-html/html-language-features/server/dist/node/htmlServerMain.js", "--stdio"},
     on_attach = require'lsp'.common_on_attach,
     capabilities = capabilities
-}:w
+}
