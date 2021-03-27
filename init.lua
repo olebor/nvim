26c0f2ad2606d1945881ba7b9e8a5b3adac760de
Ole Johnny Borgersen <olebor@gmail.com>
2021-03-27 10:54:02 +0100

    Initial stuff lsp

--- init.lua
+++ init.lua
@@ -8,11 +8,13 @@ if vim.g.vscode then
 else
 	vim.cmd('let $NVIM_TUI_ENABLE_TRUE_COLOR=1')
 	vim.cmd('source $HOME/.config/nvim/plug-config/polyglot.vim')
-	
+
 	-- General
 	vim.cmd('source $HOME/.config/nvim/vim-plug/plugins.vim')
 	vim.cmd('source $HOME/.config/nvim/general/settings.vim')
+	require('nv-globals')
 	require('keymappings')
+	require('nv-compe')
 
 	-- Theme Stuff
 	--" source $HOME/.config/nvim/themes/dracula.vim
@@ -25,15 +27,32 @@ else
 	vim.cmd('source $HOME/.config/nvim/plug-config/start-screen.vim')
 	require('nv-bufferline')
 
+	-- LSP
+	require('lsp')
+	-- require('lsp.clangd')
+	require('lsp.lua-ls')
+	-- require('lsp.bash-ls')
+	require('lsp.js-ts-ls')
+	-- require('lsp.python-ls')
+	-- require('lsp.json-ls')
+	-- require('lsp.yaml-ls')
+	-- require('lsp.vim-ls')
+	-- require('lsp.graphql-ls')
+	-- require('lsp.css-ls')
+	-- require('lsp.docker-ls')
+	-- require('lsp.html-ls')
+	-- require('lsp.efm-general-ls')
+	-- require('lsp.virtual_text')
+
 	-- " Language Tools
-	vim.cmd('source $HOME/.config/nvim/plug-config/coc/coc.vim')
-	vim.cmd('source $HOME/.config/nvim/plug-config/coc/coc-extensions.vim')
+	-- vim.cmd('source $HOME/.config/nvim/plug-config/coc/coc.vim')
+	-- vim.cmd('source $HOME/.config/nvim/plug-config/coc/coc-extensions.vim')
 	vim.cmd('source $HOME/.config/nvim/plug-config/terraform.vim')
 	vim.cmd('source $HOME/.config/nvim/plug-config/nerdcommenter.vim')
 	vim.cmd('source $HOME/.config/nvim/plug-config/rainbow.vim')
 
 	-- " File Management
-	vim.cmd('source $HOME/.config/nvim/plug-config/fzf.vim')
+	-- vim.cmd('source $HOME/.config/nvim/plug-config/fzf.vim')
 	vim.cmd('source $HOME/.config/nvim/plug-config/gitgutter.vim')
 	vim.cmd('source $HOME/.config/nvim/plug-config/vim-rooter.vim')
 	require('nv-nvimtree')
