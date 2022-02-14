79d1847dd43555b54392ed7386cd893491d98e1e
Ole Johnny Borgersen <ole@appfarm.io>
2022-02-14 22:49:39 +0100

    New bufferline config

--- lua/nv/core/bufferline.lua
+++ lua/nv/core/bufferline.lua
@@ -2,61 +2,127 @@ local M = {}
 
 M.config = function()
 	vim.o.termguicolors = true
+	local status_ok, bufferline = pcall(require, "bufferline")
+	if not status_ok then
+		return
+	end
 
-	-- colors for active , inactive buffer tabs
-	require("bufferline").setup({
+	bufferline.setup({
 		options = {
+			numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+			close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+			right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+			left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
+			middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
+			-- NOTE: this plugin is designed with this icon in mind,
+			-- and so changing this is NOT recommended, this is intended
+			-- as an escape hatch for people who cannot bear it for whatever reason
+			indicator_icon = "▎",
 			buffer_close_icon = "",
+			-- buffer_close_icon = '',
 			modified_icon = "●",
-			close_icon = "",
+			close_icon = "",
+			-- close_icon = '',
 			left_trunc_marker = "",
 			right_trunc_marker = "",
-			max_name_length = 14,
-			max_prefix_length = 13,
-			tab_size = 18,
-			enforce_regular_tabs = true,
-			view = "multiwindow",
+			max_name_length = 30,
+			max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
+			tab_size = 21,
+			diagnostics = false, -- | "nvim_lsp" | "coc",
+			diagnostics_update_in_insert = false,
+			offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+			show_buffer_icons = true,
 			show_buffer_close_icons = true,
-			separator_style = "thin",
+			show_close_icon = true,
+			show_tab_indicators = true,
+			persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+			-- can also be a table containing 2 custom separators
+			-- [focused and unfocused]. eg: { '|', '|' }
+			separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
+			enforce_regular_tabs = true,
+			always_show_bufferline = true,
 		},
 		highlights = {
-			background = {
-				guifg = comment_fg,
-				guibg = "#282c34",
-			},
 			fill = {
-				guifg = comment_fg,
-				guibg = "#282c34",
+				guifg = { attribute = "fg", highlight = "#ff0000" },
+				guibg = { attribute = "bg", highlight = "TabLine" },
 			},
-			buffer_selected = {
-				guifg = nomal_fg,
-				guibg = "#3A3E44",
-				gui = "bold",
+			background = {
+				guifg = { attribute = "fg", highlight = "TabLine" },
+				guibg = { attribute = "bg", highlight = "TabLine" },
 			},
-			separator_visible = {
-				guifg = "#282c34",
-				guibg = "#282c34",
+
+			buffer_visible = {
+				guifg = { attribute = "fg", highlight = "TabLine" },
+				guibg = { attribute = "bg", highlight = "TabLine" },
 			},
-			separator_selected = {
-				guifg = "#282c34",
-				guibg = "#282c34",
+
+			close_button = {
+				guifg = { attribute = "fg", highlight = "TabLine" },
+				guibg = { attribute = "bg", highlight = "TabLine" },
 			},
-			separator = {
-				guifg = "#282c34",
-				guibg = "#282c34",
+			close_button_visible = {
+				guifg = { attribute = "fg", highlight = "TabLine" },
+				guibg = { attribute = "bg", highlight = "TabLine" },
 			},
-			indicator_selected = {
-				guifg = "#282c34",
-				guibg = "#282c34",
+
+			tab_selected = {
+				guifg = { attribute = "fg", highlight = "Normal" },
+				guibg = { attribute = "bg", highlight = "Normal" },
+			},
+			tab = {
+				guifg = { attribute = "fg", highlight = "TabLine" },
+				guibg = { attribute = "bg", highlight = "TabLine" },
+			},
+			tab_close = {
+				guifg = { attribute = "fg", highlight = "TabLineSel" },
+				guibg = { attribute = "bg", highlight = "Normal" },
+			},
+
+			duplicate_selected = {
+				guifg = { attribute = "fg", highlight = "TabLineSel" },
+				guibg = { attribute = "bg", highlight = "TabLineSel" },
+				gui = "italic",
+			},
+			duplicate_visible = {
+				guifg = { attribute = "fg", highlight = "TabLine" },
+				guibg = { attribute = "bg", highlight = "TabLine" },
+				gui = "italic",
+			},
+			duplicate = {
+				guifg = { attribute = "fg", highlight = "TabLine" },
+				guibg = { attribute = "bg", highlight = "TabLine" },
+				gui = "italic",
+			},
+
+			modified = {
+				guifg = { attribute = "fg", highlight = "TabLine" },
+				guibg = { attribute = "bg", highlight = "TabLine" },
 			},
 			modified_selected = {
-				guifg = string_fg,
-				guibg = "#3A3E44",
+				guifg = { attribute = "fg", highlight = "Normal" },
+				guibg = { attribute = "bg", highlight = "Normal" },
+			},
+			modified_visible = {
+				guifg = { attribute = "fg", highlight = "TabLine" },
+				guibg = { attribute = "bg", highlight = "TabLine" },
+			},
+
+			separator = {
+				guifg = { attribute = "bg", highlight = "TabLine" },
+				guibg = { attribute = "bg", highlight = "TabLine" },
+			},
+			separator_selected = {
+				guifg = { attribute = "bg", highlight = "Normal" },
+				guibg = { attribute = "bg", highlight = "Normal" },
+			},
+
+			indicator_selected = {
+				guifg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
+				guibg = { attribute = "bg", highlight = "Normal" },
 			},
 		},
 	})
-
-	vim.g.mapleader = " "
 end
 
 return M
