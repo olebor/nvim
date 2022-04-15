b7bd52983011156d940bb039919a8906dc764266
Ole Johnny Borgersen <ole@appfarm.io>
2022-04-15 07:37:34 +0000

    Some cleanup of treesitter

--- lua/nv/core/treesitter.lua
+++ lua/nv/core/treesitter.lua
@@ -40,6 +40,11 @@ M.setup = function()
 			enable = true,
 			disable = {"yaml"}
 		},
+		rainbow = {
+			enable = true,
+			extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+			max_file_lines = 1000 -- Do not enable for files with more than 1000 lines, int
+		},
 		autotag = {
 			enable = false
 		},
@@ -60,29 +65,6 @@ M.setup = function()
 				["."] = "textsubjects-smart",
 				[";"] = "textsubjects-big"
 			}
-		},
-		playground = {
-			enable = false,
-			disable = {},
-			updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-			persist_queries = false, -- Whether the query persists across vim sessions
-			keybindings = {
-				toggle_query_editor = "o",
-				toggle_hl_groups = "i",
-				toggle_injected_languages = "t",
-				toggle_anonymous_nodes = "a",
-				toggle_language_display = "I",
-				focus_language = "f",
-				unfocus_language = "F",
-				update = "R",
-				goto_node = "<cr>",
-				show_help = "?"
-			}
-		},
-		rainbow = {
-			enable = true,
-			extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-			max_file_lines = 1000 -- Do not enable for files with more than 1000 lines, int
 		}
 	}
 
