206fa3ebaaab03f5921c851e65b210adbbd75e14
Ole Johnny Borgersen <ole@appfarm.io>
2022-05-06 12:28:56 +0000

    Update scripts

--- utils/appimage_update.sh
+++ utils/appimage_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L -o /tmp/nvim.appimage https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage 
+curl -L -o /tmp/nvim.appimage https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 sudo mv /tmp/nvim.appimage /usr/local/bin/nvim
 chmod +x /usr/local/bin/nvim
 
