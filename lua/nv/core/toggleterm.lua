efca769f7d84c281b246b876e9cd8163c8fcd535
Ole Johnny Borgersen <ole@appfarm.io>
2023-03-23 06:04:38 +0000

    Changed shortcut for toggle-term

--- lua/nv/core/toggleterm.lua
+++ lua/nv/core/toggleterm.lua
@@ -10,7 +10,7 @@ M.config = function()
 
 	toggleterm.setup({
 		size = 20,
-		open_mapping = [[<C-\>]],
+		open_mapping = [[<C-t>]],
 		hide_numbers = true,
 		shade_filetypes = {},
 		shade_terminals = true,
