cc1c611e5e477752cb6c977e4f104e20efd23350
Ole Johnny Borgersen <ole@appfarm.io>
2023-08-12 20:54:51 +0200

    Misc formatting

--- lua/nv/lsp/lspconfig.lua
+++ lua/nv/lsp/lspconfig.lua
@@ -1,5 +1,8 @@
+local Log = require("core.log")
+
 local status_ok, lspconfig = pcall(require, "lspconfig")
 if not status_ok then
+	Log:error("LSP Config failed")
 	return
 end
 
@@ -7,25 +10,25 @@ local handlers = require("nv.lsp.handlers")
 
 handlers.setup()
 
-lspconfig.lua_ls.setup {
+lspconfig.lua_ls.setup({
 	on_attach = handlers.on_attach,
 	settings = {
 		Lua = {
 			diagnostics = {
-				globals = { 'vim' }
-			}
-		}
-	}
-}
+				globals = { "vim" },
+			},
+		},
+	},
+})
 
-lspconfig.tsserver.setup {
-	on_attach = handlers.on_attach
-}
+lspconfig.tsserver.setup({
+	on_attach = handlers.on_attach,
+})
 
-lspconfig.terraformls.setup {
-	on_attach = handlers.on_attach
-}
+lspconfig.terraformls.setup({
+	on_attach = handlers.on_attach,
+})
 
-lspconfig.yamlls.setup {
-	on_attach = handlers.on_attach
-}
+lspconfig.yamlls.setup({
+	on_attach = handlers.on_attach,
+})
