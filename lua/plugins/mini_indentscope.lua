2b5fb1eef57b96ff836992322998e715be5d3284
Ole Johnny Borgersen <ole@appfarm.io>
2024-04-12 22:22:25 +0200

    Mini Indentscope

---
+++ lua/plugins/mini_indentscope.lua
@@ -0,0 +1,34 @@
+-- https://github.com/echasnovski/mini.indentscope
+-- Mini indentscope
+-- Will draw vertical lines to indicate the scope of the code block you are in
+
+return {
+	{
+		"echasnovski/mini.indentscope",
+		version = false,
+		event = "BufEnter",
+		opts = {
+			draw = {
+				delay = 0,
+			},
+			symbol = "│",
+			options = { try_as_border = true },
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				desc = "Disable indentscope for certain filetypes",
+				pattern = {
+					"help",
+					"lazy",
+					"mason",
+					"notify",
+					"nvimtree",
+					"dashboard",
+				},
+				callback = function()
+					vim.b.miniindentscope_disable = true
+				end,
+			})
+		end,
+	},
+}
