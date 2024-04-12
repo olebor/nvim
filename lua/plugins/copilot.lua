f0e596877250ec7e1d959dfdcf976f696fc5bc4c
Ole Johnny Borgersen <ole@appfarm.io>
2024-04-12 22:09:08 +0200

    Moved copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,21 @@
+-- Copilot
+
+return {
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = { "BufEnter" },
+		config = function()
+			require("copilot").setup({
+				suggestion = { enabled = false },
+				panel = { enabled = false },
+			})
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	},
+}
