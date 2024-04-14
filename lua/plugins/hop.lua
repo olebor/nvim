c1ebefff546f3485b80c186dcc98fd04ecb8f5d3
Ole Johnny Borgersen <ole@appfarm.io>
2024-04-14 21:37:01 +0200

    cleanup hop

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,17 +1,15 @@
 -- Hop to anywhere
 
-function configHop()
-	vim.api.nvim_set_keymap("n", "s", ":HopChar1<cr>", {
-		silent = true,
-	})
-	vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", {
-		silent = true,
-	})
-
-	require("hop").setup()
-end
-
 return {
 	"phaazon/hop.nvim",
-	config = configHop,
+	config = function()
+		vim.api.nvim_set_keymap("n", "s", ":HopChar1<cr>", {
+			silent = true,
+		})
+		vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", {
+			silent = true,
+		})
+
+		require("hop").setup()
+	end,
 }
