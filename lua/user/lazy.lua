5fc2fa7593515c41c91b95aec7aa76c4728e01c9
Ole Johnny Borgersen <ole@appfarm.io>
2024-04-15 07:54:56 +0200

    Surround

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -75,16 +75,6 @@ local plugins = {
 	},
 
 	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end,
-	},
-	{
 		-- Treesitter
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
